8e262a4a2a8e50c6598d592f275b2e327a8c3b3c
facccge <fcfc5702@qq.com>
2019-04-24 14:42:08 +0800

    Updated EmployeeService and create EmployeeController

--- src/main/java/com/tw/springbootdemo/service/EmployeeService.java
+++ src/main/java/com/tw/springbootdemo/service/EmployeeService.java
@@ -4,22 +4,23 @@ import com.tw.springbootdemo.domain.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeService {
+    /**
+     * 创建线程安全的Map
+     */
+    private static Map<Integer, Employee> employees = Collections.synchronizedMap(new HashMap<>());
 
-    private static List<Employee> employees = new LinkedList<>();
-    static{
-        employees.add(new Employee(0,"小明",20,"男"));
-        employees.add(new Employee(1,"小红",19,"女"));
-        employees.add(new Employee(2,"小智",15,"男"));
-        employees.add(new Employee(3,"小刚",16,"男"));
+    static {
+        employees.put(0,new Employee(0, "小明", 20, "男"));
+        employees.put(1,new Employee(1, "小红", 19, "女"));
+        employees.put(2,new Employee(2, "小智", 15, "男"));
+        employees.put(3,new Employee(3, "小刚", 16, "男"));
     }
 
-
-    public List<Employee> getEmployees(){
-        return employees;
+    public List<Employee> getEmployees() {
+        return  new ArrayList<>(employees.values());
     }
 }
