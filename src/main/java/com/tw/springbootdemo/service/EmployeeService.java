5e95bdcf4b2406625ad628f7b16e7cd917ed3522
facccge <fcfc5702@qq.com>
2019-04-24 15:36:46 +0800

    Completed EmployeeController and test cases

--- src/main/java/com/tw/springbootdemo/service/EmployeeService.java
+++ src/main/java/com/tw/springbootdemo/service/EmployeeService.java
@@ -18,6 +18,7 @@ public class EmployeeService {
         employees.put(1,new Employee(1, "小红", 19, "女"));
         employees.put(2,new Employee(2, "小智", 15, "男"));
         employees.put(3,new Employee(3, "小刚", 16, "男"));
+        employees.put(4,new Employee(4, "小霞", 15, "女"));
     }
 
     public List<Employee> getEmployees() {
